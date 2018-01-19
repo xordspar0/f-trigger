752c3b2c2f7eb12c991762298db24e02fecad96f
Jordan Christiansen <Jordan.Christiansen@target.com>
2018-01-19 16:04:01 -0600

    Format with `go fmt`

--- main.go
+++ main.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "time"
+	"bufio"
+	"fmt"
+	"os"
+	"time"
 
-    "github.com/urfave/cli"
-    "golang.org/x/crypto/ssh/terminal"
+	"github.com/urfave/cli"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-func main()  {
-    app := cli.NewApp()
-    app.Name = "f-trigger"
-    app.Usage = "Trigger events based on a frequency on stdin"
-    app.Action = run
+func main() {
+	app := cli.NewApp()
+	app.Name = "f-trigger"
+	app.Usage = "Trigger events based on a frequency on stdin"
+	app.Action = run
 
-    app.Flags = []cli.Flag{
-        cli.StringFlag{
-            Name: "delimiter, d",
-            Value: "\n",
-            Usage: "This character determines the start and end of each timed unit (not yet implemented)",
-        },
-    }
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "delimiter, d",
+			Value: "\n",
+			Usage: "This character determines the start and end of each timed unit (not yet implemented)",
+		},
+	}
 
-    if err := app.Run(os.Args); err != nil {
+	if err := app.Run(os.Args); err != nil {
 		fmt.Println(err.Error())
-        os.Exit(1)
-    }
+		os.Exit(1)
+	}
 }
 
 func run(c *cli.Context) error {
-    var err error
+	var err error
 
-    lastTime := time.Now()
+	lastTime := time.Now()
 
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    var bmpFormat string
-    if terminal.IsTerminal(int(os.Stdout.Fd())) {
-        bmpFormat = "\r\033[K%.0f bmp"
-    } else {
-        bmpFormat = "%.0f bmp\n"
-    }
+	var bmpFormat string
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		bmpFormat = "\r\033[K%.0f bmp"
+	} else {
+		bmpFormat = "%.0f bmp\n"
+	}
 
-    for scanner.Scan() {
-        fmt.Printf(bmpFormat, 1 / time.Now().Sub(lastTime).Minutes())
-        lastTime = time.Now()
-    }
-    if err = scanner.Err(); err != nil {
-        fmt.Fprintln(os.Stderr, "Error reading input: ", err.Error())
-    }
+	for scanner.Scan() {
+		fmt.Printf(bmpFormat, 1/time.Now().Sub(lastTime).Minutes())
+		lastTime = time.Now()
+	}
+	if err = scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input: ", err.Error())
+	}
 
-    return err
+	return err
 }
